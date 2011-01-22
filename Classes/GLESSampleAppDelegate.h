70adfb7e158d0a6fc63ab72814ae491014556cee
Zach Margolis <zbmargolis@gmail.com>
2011-01-22 12:25:03 -0800

    Initial commit

---
+++ Classes/GLESSampleAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  GLESSampleAppDelegate.h
+//  GLESSample
+//
+//  Created by Zach Margolis on 1/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLESSampleAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
